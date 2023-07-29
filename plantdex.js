ad2c0ac50e7a28be29c0fb7cc26a42f0fcd1791c
User5678953 <kendynweb@gmail.com>
2023-07-29 12:43:13 -0400

    user can query api with any string input"

--- plantdex.js
+++ plantdex.js
@@ -1,16 +1,28 @@
-alert('You are connected!')
+alert('You are connected!') // Test CSS and HTML connection in browser
+
 // API KEY = sk-kzbV64c08c9a33a321675
 const apiURL = 'https://perenual.com/api/species-list?page=1&key=sk-kzbV64c08c9a33a321675'
 
-fetch('https://perenual.com/api/species-list?page=1&key=sk-kzbV64c08c9a33a321675')
-  .then (response => {
-    return response.json() 
-  })
-  .then (data => {
-    console.log(data) // Log the parsed data to the console
-  })
-  .catch (error => {
-    console.error('Error fetching data:', error);
-  })
+// DOM References
+const searchInput = document.getElementById('userSearchInput')
+const searchButton = document.getElementById('searchButton')
 
-  
+// Event listener for search button 'click'
+searchButton.addEventListener('click', () => {
+  const searchTerm = searchInput.value.trim();
+  console.log('Search Term:', searchTerm);
+
+  // Logic to fetch API info based on user input
+  fetch(apiURL + `&q=${searchTerm}`)
+    .then(response => {
+      return response.json(); // call json file
+    })
+    .then(data => {
+      console.log('API Response:', data);
+    })
+    .catch(error => {
+      console.error('Error Fetching Data', error);
+    });
+});
+
+//TESTED!!!! API results are able to be console.logged based on user input 
