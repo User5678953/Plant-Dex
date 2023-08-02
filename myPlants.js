74f4e4a8382681dd3240846f2e9437fc9890f187
User5678953 <kendynweb@gmail.com>
2023-08-01 21:37:08 -0400

    broke code attempting to make carousel, restored original version, will bang my head and talk to a rubber duck

---
+++ myPlants.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+   
+    // Function to load and display the saved plants from storage (implement this)
+    function loadAndDisplayMyPlants() {
+      // ... (implement the logic to retrieve and display saved plants)
+    }
+  
+    // Call the function to load and display saved plants on page load
+    loadAndDisplayMyPlants()
+  }
+  
